2913e1a17225e1e83b3fc7a7470cd1b74196cf5b
Boris Xiao <borishaw@gmail.com>
2017-04-11 18:24:16 +0000

    exercise 1

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # A sample Gemfile
-source "https://rubygems.org"
+source "https://rubygems.org" do
 
-gem 'pry' # for debugging
-gem 'pg'
-gem 'activerecord'
+  gem 'pry' # for debugging
+  gem 'pg'
+  gem 'activerecord'
+
+end
--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+end
+
+burnaby = Store.new
+
+burnaby.name = 'Burnaby'
+burnaby.annual_revenue = 300000
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+
+burnaby.save
+
+Store.create(name: 'Richmond',
+             annual_revenue: 1260000,
+             mens_apparel: false,
+             womens_apparel: true)
+
+Store.create(name: 'Gastown',
+             annual_revenue: 190000,
+             mens_apparel: true,
+             womens_apparel: false)
